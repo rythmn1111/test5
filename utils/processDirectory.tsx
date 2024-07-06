6ac4b855f6940dd1d827566b86a1603385d7149a
rythmn1111 <rythmnmagnani0@gmail.com>
2024-07-06 12:10:54 +0530

    Built Process Loader

--- utils/processDirectory.tsx
+++ utils/processDirectory.tsx
@@ -1,8 +1,9 @@
 import dynamic from "next/dynamic";
 import HelloWorld from "../components/apps/helloworld";
 import { Component } from "react";
+import { Processes } from "../types/processDirectory";
 
-const processDirectory = {
+const processDirectory: Processes = {
     HelloWorld: {
         Component: dynamic(() => import("../components/apps/helloworld")),
     }
