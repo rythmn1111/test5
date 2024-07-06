8a184f2893b6e6140568e4afe93bba44151826a9
rythmn1111 <rythmnmagnani0@gmail.com>
2024-07-06 17:15:28 +0530

    not synced properly

--- contexts/process.tsx
+++ contexts/process.tsx
@@ -1,12 +1,18 @@
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import processDirectory from '../utils/processDirectory';
+import { createContext } from 'vm';
 
-const ProcessLoader: FC = () => (
-  <>
+const ProcessContext = createContext({});
+
+const ProcessLoader: FC = () => {
+  const [processes]= useState(processDirectory);
+  return(
+    <>
     {Object.entries(processDirectory).map(([id, { Component }]) => (
       <Component key={id} />
     ))}
-  </>
+    </>
 );
+}
 
 export default ProcessLoader;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
 import { FC } from "react";
-import processDirectory from "../../utils/processDirectory";
-const inter = Inter({ subsets: ["latin"] });
+import ProcessLoader from "../../contexts/process";
+
+
+
 
 export default function Home() {
-  return <processDirectory.HelloWorld.Component />;
+  return <ProcessLoader />;
 }
